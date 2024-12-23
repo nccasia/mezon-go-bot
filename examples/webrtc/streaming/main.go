0fcf93122781b8bc0ef8c4f4a404fa71686b1c5b
thanhlong1203 <longma350@gmail.com>
2024-12-23 18:06:56 +0700

    fix path

--- examples/webrtc/streaming/main.go
+++ examples/webrtc/streaming/main.go
@@ -19,22 +19,28 @@ func main() {
 
 func Streaming() {
 
-	clanId := "3456110592"    // KOMU_2
-	channelId := "4311748608" // NCC8
-	userId := "4198400"       // longma350
-	displayName := "BOT350"   // longma350
+	clanId := "1775732550744936448"    // KOMU
+	channelId := "1837040466697129984" // NCC8
+	userId := "1835944289075466240"    // komu
+	// clanId := "3456110592"    // KOMU_2
+	// channelId := "4311748608" // NCC8
+	// userId := "4198400"       // longma350
+	displayName := "KOMU" // longma350
 	wsConn, err := radiostation.NewWSConnection(&configs.Config{
-		BasePath:     "stn.nccsoft.vn",
+		BasePath: "stn.nccsoft.vn",
+		// BasePath:     "localhost:8081",
 		Timeout:      10,
 		InsecureSkip: true,
-		UseSSL:       true,
+		// UseSSL:       true,
+		UseSSL: false,
 	}, clanId, channelId, userId, displayName)
 	if err != nil {
 		panic(err)
 	}
 
 	// ffmpeg -i test.mp3 -c:a libopus -page_duration 20000 test.ogg;
-	filePath := "../../audio/ncc8_example.ogg"
+	// filePath := "../../audio/ncc8_example.ogg"
+	filePath := "../../audio/lk_thucuoi.ogg"
 	rtcConn, err := rtc.NewStreamingRTCConnection(webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
 			{
