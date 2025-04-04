6ce91a852a0e12811eed3194238ad6e965b595ec
thanhlong1203 <longma350@gmail.com>
2025-04-04 10:36:52 +0700

    add ncc8 218

--- command.go
+++ command.go
@@ -337,7 +337,7 @@ func ScheduleFridayAudio() {
 
 		// Prepare audio file and episode details
 		// fileNCC8Path := filepath.Join(constants.NCC8_AUDIO_DIR, ncc8AudioName)
-		episodeText := fmt.Sprintf("NCC8 số %d đang được phát trên ", 217)
+		episodeText := fmt.Sprintf("NCC8 số %d đang được phát trên ", 218)
 		length := len(episodeText)
 		content := fmt.Sprintf("{\"t\":\"%s\",\"hg\":[{\"channelid\":\"%s\",\"s\":%d,\"e\":%d}]}", episodeText, cfg.Ncc8ChannelId, length, length+10)
 
@@ -345,7 +345,7 @@ func ScheduleFridayAudio() {
 		bot.SendMessage(nil, content, cfg.Ncc8ChannelId)
 
 		// Play the audio
-		err := player.Play("./audio/ncc8_217.ogg")
+		err := player.Play("./audio/ncc8_218.ogg")
 		if err != nil {
 			bot.Logger().Error("[ncc8] failed to play audio from URL", zap.String("url", ncc8AudioName), zap.Error(err))
 			return
